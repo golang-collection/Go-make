e5f112fe91e0afb2e8101720906eefe70bd727e4
super <supeng842499467@gmail.com>
2020-08-29 17:00:05 +0800

    init

--- main.go
+++ main.go
@@ -1,14 +1,34 @@
 package main
 
-import "fmt"
+import (
+	v "Go-make/version"
+	"encoding/json"
+	"fmt"
+	"github.com/spf13/pflag"
+	"os"
+)
 
 /**
 * @Author: super
 * @Date: 2020-08-29 16:42
-* @Description:
+* @Description: 支持显示版本信息
 **/
+var (
+	version = pflag.BoolP("version", "v", false, "show version info.")
+)
 
 func main() {
-	fmt.Println("hello")
-	fmt.Println("world")
+	fmt.Println("hello world")
+	pflag.Parse()
+	if *version {
+		v := v.Get()
+		marshalled, err := json.MarshalIndent(&v, "", "  ")
+		if err != nil {
+			fmt.Printf("%v\n", err)
+			os.Exit(1)
+		}
+
+		fmt.Println(string(marshalled))
+		return
+	}
 }
---
+++ version/doc.go
@@ -0,0 +1,7 @@
+package version
+
+/**
+* @Author: super
+* @Date: 2020-08-26 20:44
+* @Description:
+**/
